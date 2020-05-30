35b5944689c3bc2baf452a484f35af3bc221dd74
ALI ZIA KHAN <59319815+ALI-ZIA-KHAN@users.noreply.github.com>
2020-05-31 02:50:12 +0500

    Add files via upload
    Basic attributes of Random Library

---
+++ Random module.py
@@ -0,0 +1,18 @@
+from random import*
+#in between in two
+x=uniform(0,1)
+print(x)
+
+#shuffling
+l1=[10,20,30,40,50]
+f=shuffle(l1)
+print(l1)
+
+#choicing
+n=["ALI","SARA","ZAIN","ZARA","TALHA"]
+y=choice(n)
+print(y)
+
+#selecting in particular no.s
+z=sample(n,2)
+print(z)
