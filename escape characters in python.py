6698d8b4f867ae1fa58b5a39d6014e47edb8baf1
ALI ZIA KHAN <59319815+ALI-ZIA-KHAN@users.noreply.github.com>
2020-05-31 02:48:53 +0500

    Add files via upload
    Basic knowledge of escape characters in python.

---
+++ escape characters in python.py
@@ -0,0 +1,17 @@
+#escape characters
+print("HELLO WORLD")
+#\t for a tab space
+print("Hello\tworld")
+#\n for new line
+print("hello\nworld")
+#end="\n" for new line(u can give more than 1 times)
+print("hello world",end="\n")
+print("hello world")
+#end="\t" for a tab space(u can give more than 1 times)
+print("hello world",end="\t\t")
+print("hello world")
+#end="" keeps on same line
+print("hello world",end="")
+print("pakistan")
+#\b removes the single character or backspace
+print("Hello \bWorld!")
