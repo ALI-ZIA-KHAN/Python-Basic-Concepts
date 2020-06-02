7c35d2f8684342e4d63e6472961e1a8482341f65
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-03 01:54:37 +0500

    FOR N WHILE LOOP BASIC CONCEPT

---
+++ FOR n WHILE LOOP.py
@@ -0,0 +1,20 @@
+from math import*
+i=1
+while(i<=100):
+    print(i)
+    i=i+1
+
+j=100
+while(j>=0):
+    print(j)
+    j=j-1
+
+num=int(input("Enter range"))
+for m in range(1,num):
+    print(m)
+
+
+
+
+for l in range(10):
+    print(l)
