ad150ab18ddc4ca001eaef6505d3747ed224a9a3
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-29 01:10:05 +0500

    function part1

---
+++ function 1.py
@@ -0,0 +1,17 @@
+#functions
+
+
+##1st
+def add():
+    n1=input("fist name")
+    n2=input("nationality")
+    print(n1+" "+n2)
+add()
+
+
+#output
+#fist nameAli
+#nationalityPakistani
+#Ali Pakistani
+#
+
