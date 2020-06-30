d22fb22a6268f9667db0cc919e454092a68b3c35
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-30 15:33:23 +0500

    function with return statement

---
+++ function 6.py
@@ -0,0 +1,17 @@
+##passsing info from back from them
+
+##using return statement
+
+def add(l1,l2):
+    c=l1+l2
+    return c,"pak"
+result=add(5,5)
+print(result)
+
+
+rs=add(6,9)
+print("hello",rs)
+
+#output
+#1-(10, 'pak')
+#2-hello (15, 'pak')
