597476de1bc4339026180b020260a928eca1a805
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-30 15:29:57 +0500

    positional arguments

---
+++ function 2.py
@@ -0,0 +1,10 @@
+from math import*
+##positional arguments
+def subt(m1,m2):
+    print("the ans is:",m1-m2)
+
+subt(5,3)#2
+subt(6,7)#-1
+
+#positional arguments:arguments will be assign to
+#parameters a/c to their position
