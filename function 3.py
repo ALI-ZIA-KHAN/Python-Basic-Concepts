0a4386e2ee3d3abc2caacb5300db80487121640d
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-30 15:30:38 +0500

    keyword arguments

---
+++ function 3.py
@@ -0,0 +1,15 @@
+##key word arguments
+
+def fullname(first,last,middle):
+    print(first+" "+middle+" "+last)
+fullname(first="Ali",middle="Zia",last="Khan")
+##argumetnts will be assigned to parameters
+#by matching the keyword
+
+
+##also
+#if using key word and positional both so keyword must be at last
+fullname("Shahid","Afridi",middle="Khan")
+
+#output-1 Ali Zia Khan
+#output-2 Shahid Khan Afridi
